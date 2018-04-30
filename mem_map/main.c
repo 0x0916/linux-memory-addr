b57e4ec844158e6cd0a2b023c37fa125b1d9382b
Wang Long <w@laoqinren.net>
2018-04-30 18:54:19 +0800

    update mem_map
    Signed-off-by: Wang Long <w@laoqinren.net>

--- mem_map/main.c
+++ mem_map/main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
 	unsigned long tmp;
-	tmp = 0x12345678;
+	int fd = -1, len = 0;
+	char buf[4096];
+
+	tmp = 0x12345678deadbeaf;
 	printf("tmp variable address: 0x%08lX\n", &tmp);
+
+	if ((fd = open("/proc/sys_reg", O_RDONLY)) == -1) {
+		fprintf(stderr, "Open /proc/sys_reg file failed!\n");
+		fflush(NULL);
+		exit(102);
+	}
+
+	if ((len = read(fd, buf, sizeof(buf) - 1)) < 0) {
+		perror("/proc/sys_reg");
+		fflush(NULL);
+		exit(103);
+	}
+
+	buf[len] = 0;
+
+	printf("%s", buf);
+
+	// loop until we type Ctrl+C
+	while(1);
+
 	return 0;
 }
